5e1a97cd552582df236ea26aba501640737430c2
Andrew Souther <asouther@fordham.edu>
2021-06-19 18:01:36 -0500

    added outline of all four steps, filled in proof for step 4

--- src/arrows_theorem.lean
+++ src/arrows_theorem.lean
@@ -1,6 +1,7 @@
 import logic.relation
 import data.finset.basic
 import data.set.finite
+import tactic
 
 open relation
 open vector
@@ -36,12 +37,16 @@ begin
     rw or_iff_not_imp_right at hR, exact hR hyp,
 end
 
-lemma nP_of_reverseP {R : σ → σ → Prop} (x y : σ) (h : P R x y): ¬(P R y x) :=
+lemma nP_of_reverseP {R : σ → σ → Prop} {x y : σ} (h : P R x y): ¬(P R y x) :=
 begin
     unfold P, push_neg,
     intro n, 
     exact h.1,
 end 
+
+lemma false_of_P_self {R : σ → σ → Prop} {x : σ} : P R x x → false := 
+by simp only [P, forall_false_left, and_not_self]
+
 
 --what it means for social states to have the "same order" with respect to two relations
 def same_order (R R': σ → σ → Prop) (x y x' y' : σ) : Prop :=
@@ -60,8 +65,8 @@ begin
     split,
     exact ⟨ (λ h, hR'), (λ h, hR)⟩,
     split,
-    intro h, exfalso, exact (nP_of_reverseP x y hR) h,
-    intro h, exfalso, exact (nP_of_reverseP x y hR') h,
+    intro h, exfalso, exact (nP_of_reverseP hR) h,
+    intro h, exfalso, exact (nP_of_reverseP hR') h,
 end
 
 lemma same_order_of_reverseP_P' {R R': σ → σ → Prop} {x y : σ} (hR : P R y x) (hR' : P R' y x) : 
@@ -72,8 +77,8 @@ begin
     intro h, exfalso, exact hR'.2 h,
     exact ⟨ (λ h, hR'.1), (λ h, hR.1)⟩,
     split, split,
-    intro h, exfalso, exact (nP_of_reverseP x y hR) h,
-    intro h, exfalso, exact (nP_of_reverseP x y hR') h,
+    intro h, exfalso, exact (nP_of_reverseP hR) h,
+    intro h, exfalso, exact (nP_of_reverseP hR') h,
     exact ⟨ (λ h, hR'), (λ h, hR)⟩,
 end
 
@@ -115,6 +120,9 @@ def is_arrovian (f : (ι → σ → σ → Prop) → (σ → σ → Prop) ) (X :
     unrestricted_domain f ∧ weak_pareto f X N ∧ ind_of_irr_alts f X N
     ∧ ∀ (Rᵢ : (ι → σ → σ → Prop)), is_pref_ordering (f Rᵢ)
 
+def is_dictatorship (f : (ι → σ → σ → Prop) → (σ → σ → Prop) ) (X : finset σ) (N : finset ι) : Prop :=
+    ∃ i ∈ N, ∀ (x y ∈ X) (Rᵢ : ι → (σ → σ → Prop)), P (Rᵢ i) x y → P (f Rᵢ) x y
+
 
 
 
@@ -122,7 +130,7 @@ def is_arrovian (f : (ι → σ → σ → Prop) → (σ → σ → Prop) ) (X :
 --If every individual in society places a social state at the top or bottom of their social preferences,
 --then society must place that social state at the top or bottom of its social preference. 
 -------------------------------------------
-lemma extremal_soc_of_extremal_indiv {f : (ι → σ → σ → Prop) → (σ → σ → Prop)}
+lemma first_step {f : (ι → σ → σ → Prop) → (σ → σ → Prop)}
     (hf : is_arrovian f X N)
     (hX : finset.card X ≥ 3)
     (b : σ) (b_in : b ∈ X ) (R : (ι → σ → σ → Prop) )
@@ -187,4 +195,189 @@ begin
     have same_order_bc := hf.2.2.1 R R₂ b c b_in c_in (λ i i_in, (hR₂ i i_in).2.2), 
     have h_trans := (hf.2.2.2 (R₂)).2.2 (same_order_ba.1.2.1 ha) (same_order_bc.1.1.1 hc),
     exact h_pareto.2 h_trans,
+end
+
+lemma vector_of_finset {α : Type} {n : ℕ} (S : finset α) (h : finset.card S = n) :
+    ∃ v : vector α n, ∀ a ∈ S, ∃ k : fin n, vector.nth v k = a :=
+begin
+  rcases S with ⟨⟨l⟩, (hl : l.nodup)⟩,
+  change l.length = n at h,
+  refine ⟨⟨l, h⟩, λ a ha, _⟩,
+  rcases list.nth_le_of_mem ha with ⟨k, hk, e⟩,
+  refine ⟨⟨k, by rwa ← h⟩, e⟩,
+end
+
+
+lemma nipkow_lemma_2 {f : (ι → σ → σ → Prop) → (σ → σ → Prop)}
+    (hf : is_arrovian f X N)
+    (hX : finset.card X ≥ 3) 
+    (hN : finset.card N ≥ 2) :
+    ∀ b ∈ X, (∃ Rᵢ : (ι → σ → σ → Prop), 
+    ∀ i ∈ N, is_extremal (Rᵢ i) X b) → 
+    ∃ (n' ∈ N) (R₁ R₂ : ι → σ → σ → Prop),
+    ( ∀ i ∈ N, i ≠ n' → ∀ x y ∈ X, same_order (R₁ i) (R₂ i) x y x y) ∧ 
+    ( ∀ i ∈ N, ∀ i ∈ N, is_extremal (R₁ i) X b ) ∧ 
+    ( ∀ i ∈ N, ∀ i ∈ N, is_extremal (R₂ i) X b ) ∧
+    ( ∀ a ∈ X, a ≠ b → P (R₁ n') a b  ) ∧
+    ( ∀ a ∈ X, a ≠ b → P (R₂ n') b a  ) ∧
+    ( ∀ a ∈ X, a ≠ b → P (f R₁) a b  ) ∧
+    ( ∀ a ∈ X, a ≠ b → P (f R₂) b a  ) := 
+begin
+    intros b b_in hRᵢ,
+    cases hRᵢ with Rᵢ hRᵢ,
+    sorry,
+end
+
+
+def is_pivotal  (f : (ι → σ → σ → Prop) → (σ → σ → Prop)) (N : finset ι) (X : finset σ) 
+    (n : ι) (b : σ) : Prop := 
+    ∃ (R₁ R₂ : ι → σ → σ → Prop),
+    ( ∀ i ∈ N, i ≠ n → ∀ x y ∈ X, same_order (R₁ i) (R₂ i) x y x y) ∧ 
+    ( ∀ i ∈ N,  is_extremal (R₁ i) X b ) ∧ 
+    ( ∀ i ∈ N,  is_extremal (R₂ i) X b ) ∧
+    ( ∀ a ∈ X, a ≠ b → P (R₁ n) a b  ) ∧
+    ( ∀ a ∈ X, a ≠ b → P (R₂ n) b a  ) ∧
+    ( ∀ a ∈ X, a ≠ b → P (f R₁) a b  ) ∧
+    ( ∀ a ∈ X, a ≠ b → P (f R₂) b a  )
+
+lemma second_step {f : (ι → σ → σ → Prop) → (σ → σ → Prop)}
+    (hf : is_arrovian f X N)
+    (hX : finset.card X ≥ 3) 
+    (hN : finset.card N ≥ 2) :
+    ∀ b ∈ X, ∃ (n' ∈ N), is_pivotal f N X n' b
+    := 
+begin
+    sorry,
+end
+
+
+def is_dictator_except (f : (ι → σ → σ → Prop) → (σ → σ → Prop))
+    (N : finset ι) (X : finset σ) (n : ι) (b : σ) : Prop := 
+    ∀ a ∈ X, ∀ c ∈ X, a ≠ b → c ≠ b → 
+    ∀ Rᵢ : ι → σ → σ → Prop, P (Rᵢ n) c a → P (f Rᵢ) c a
+
+lemma third_step {f : (ι → σ → σ → Prop) → (σ → σ → Prop)}
+    (hf : is_arrovian f X N)
+    (hX : finset.card X ≥ 3)
+    (hN : finset.card N ≥ 2) :
+    ∀ b ∈ X, ∀ n' ∈ N, is_pivotal f N X n' b →
+    is_dictator_except f N X n' b :=
+begin
+    sorry
+end
+
+
+lemma third_distinct_mem {X : finset σ} {a b : σ} ( hX : finset.card X ≥ 3) 
+        (a_in : a ∈ X) (b_in : b ∈ X) (h : a ≠ b): 
+        ∃ c ∈ X, c ≠ a ∧ c ≠ b :=
+begin
+    have h₁ := card_erase_of_mem b_in,
+    have h₂ := mem_erase_of_ne_of_mem h a_in,
+    have h₃ := card_erase_of_mem h₂,
+    rw h₁ at h₃,
+    have h₄ := nat.pred_le_pred hX,
+    simp at h₄,
+    have h₅ := nat.pred_le_pred h₄,
+    rw ← h₃ at h₅,
+    simp at h₅,
+    have h₆ : 0 < ((X.erase b).erase a).card := by linarith,
+    rw card_pos at h₆,
+    cases h₆ with c hc,
+    simp only [mem_erase, ne.def] at hc,
+    use c,
+    rw [← ne.def, ← ne.def] at hc,
+    exact ⟨hc.2.2, hc.1, hc.2.1⟩,
+end
+
+
+lemma fourth_step {f : (ι → σ → σ → Prop) → (σ → σ → Prop)}
+    (hf : is_arrovian f X N)
+    (hX : finset.card X ≥ 3) 
+    (hN : finset.card N ≥ 2)
+    (h : ∀ b ∈ X, ∃ (n' ∈ N), is_pivotal f N X n' b )
+    : is_dictatorship f X N := 
+begin
+    have X_pos : 0 < finset.card X := by linarith,
+    obtain ⟨b, b_in⟩ := (card_pos.1 X_pos).bex,
+    obtain ⟨i, i_in, i_piv⟩ := h b b_in,
+    have i_dict := third_step hf hX hN b b_in i i_in i_piv,
+    rcases i_piv with ⟨R, R', hi₁, hi₂, hi₃, hi₄, hi₅, hi₆, hi₇⟩,
+    have : ∀ a ∈ X, a ≠ b → ∀ Rᵢ : (ι → σ → σ → Prop), 
+           (P (Rᵢ i) a b → P (f Rᵢ) a b) ∧
+           (P (Rᵢ i) b a → P (f Rᵢ) b a),
+        {  
+            intros a a_in ha Rᵢ,
+            obtain ⟨c, c_in, not_a, not_b⟩ : ∃ c ∈ X, c ≠ a ∧ c ≠ b := 
+                                            third_distinct_mem hX a_in b_in ha,
+            obtain ⟨j, j_in, j_piv⟩ := h c c_in,
+            have j_dict := third_step hf hX hN c c_in j j_in j_piv, 
+            have hij : i = j,
+            {
+                by_contradiction hij,
+                have jab : P (R j) a b,
+                {   
+                    by_contradiction bla,
+                    have foo := or.resolve_left (hi₂ j j_in).2,
+                    simp only [and_imp, exists_prop, ne.def, 
+                               exists_imp_distrib, not_forall] at foo,
+                    specialize foo a a_in ha bla a a_in ha,
+                    specialize j_dict a a_in b b_in (ne_comm.1 not_a) (ne_comm.1 not_b) R foo,
+                    specialize hi₆ a a_in ha,
+                    apply nP_of_reverseP hi₆,
+                    exact j_dict,
+                }, 
+                specialize hi₁ j j_in (ne_comm.1 hij) a b a_in b_in,
+                specialize hi₇ a a_in ha,
+                specialize j_dict b b_in a a_in (ne_comm.1 not_b) (ne_comm.1 not_a) R' 
+                           (hi₁.2.1.1 jab),
+                apply nP_of_reverseP hi₇,
+                exact j_dict,
+            },
+        
+        split,
+        {
+            rw ← hij at j_dict,
+            exact j_dict b b_in a a_in (ne_comm.1 not_b) (ne_comm.1 not_a) Rᵢ,
+        },
+        {
+            rw hij,
+            exact j_dict a a_in b b_in (ne_comm.1 not_a) (ne_comm.1 not_b) Rᵢ,
+        }  },
+    use i, split, exact i_in,
+    intros x y x_in y_in Rᵢ hyp,
+    by_cases hx : x = b,
+    {
+        by_cases hy : y = b,
+        {
+            rw [hx,hy] at hyp,
+            exfalso,
+            exact false_of_P_self hyp,
+
+        },
+        {   
+            rw ← hx at *, rw ← ne.def at hy,
+            exact (this y y_in hy Rᵢ).2 hyp,
+        }, 
+    },
+    {
+        by_cases hy : y = b,
+        {
+            rw ← ne.def at hx, rw ← hy at *,
+            exact (this x x_in hx Rᵢ).1 hyp,
+        },
+        {
+            exact i_dict y y_in x x_in hy hx Rᵢ hyp,
+        },
+    },
+end
+
+
+lemma arrows_theorem {f : (ι → σ → σ → Prop) → (σ → σ → Prop)}
+    (hf : is_arrovian f X N)
+    (hX : finset.card X ≥ 3) 
+    (hN : finset.card N ≥ 2):
+    is_dictatorship f X N := 
+begin
+    apply fourth_step hf hX hN,
+    exact second_step hf hX hN,
 end

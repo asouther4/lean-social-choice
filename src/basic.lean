3a83a736ca0a254a2fcd911d41cef237ae8408d9
Andrew Souther <asouther@fordham.edu>
2021-06-29 23:27:15 -0500

    minor changes to `basic`

--- src/basic.lean
+++ src/basic.lean
@@ -36,7 +36,6 @@ not_and_not_right.mpr $ λ n, h.1
 lemma false_of_P_self (h : P R x x) : false := 
 (and_not_self _).mp h
 
-
 --what it means for social states to have the "same order" with respect to two relations
 def same_order (R R' : σ → σ → Prop) (x y x' y' : σ) : Prop :=
 ((R x y ↔ R' x' y') ∧ (R y x ↔ R' y' x')) ∧ (P R x y ↔ P R' x' y') ∧ (P R y x ↔ P R' y' x')
@@ -54,7 +53,7 @@ def is_maximal_element (x : σ) (S : finset σ) (R : σ → σ → Prop) : Prop 
 ¬ ∃ y ∈ S, P R y x
 
 def is_best_element (x : σ) (S : finset σ) (R : σ → σ → Prop) : Prop :=
-∀ y ∈ S, R y x
+∀ y ∈ S, R x y
 
 noncomputable def maximal_set (S : finset σ) (R: σ → σ → Prop) : finset σ := 
 { x ∈ S | is_maximal_element x S R }
@@ -79,33 +78,47 @@ begin
   {exact ⟨_, hb, ba.trans ed⟩}, {exact ⟨_, he, ed⟩}
 end
 
+/- Sen's Theorem on the existence of a choice function, from 
+*Social Choice and Collective Welfare* (1970). 
 
+If a relation is reflexive and total, then acyclicality is a necessary 
+and sufficient condition for a choice function to be defined on every finset `X`. 
+-/
 theorem best_elem_iff_acyclical [fintype σ] 
 (hrfl : reflexive R) (htot : total R) : 
 (∀ X : finset σ, X.nonempty → ∃ b ∈ X, is_best_element b X R) ↔ (acyclical R) := 
 begin
   split,
-  { /-
-    intro h_best,
+  { intro h_best,
     unfold acyclical, 
     by_contradiction h,
     rcases h with ⟨x, hx⟩,
-    let S : finset σ := {a ∈ univ | trans_gen (P R) a a },
-    specialize h_best S,
+    let S : finset σ := {a ∈ univ | trans_gen (P R) a x ∧ trans_gen (P R) x a},
+    have S_ne : S.nonempty :=
+      ⟨x , by simpa only [S, true_and, sep_def, mem_filter, mem_univ, and_self]⟩,
+    specialize h_best S S_ne,
     rcases h_best with ⟨b, b_in, hb⟩,
-    have x_in : x ∈ S := by simp only [S, true_and, sep_def, mem_filter, mem_univ];
-      exact hx, 
     simp only [S, true_and, sep_def, mem_filter, mem_univ] at b_in,
-    rw trans_gen.tail'_iff at b_in,
-    rcases b_in with ⟨c, hc₁, hc₂⟩,
-    specialize hb,-/
-    sorry,  },
-  { by_cases h : ∀ a c : σ, R a c ∧ R c a,
-    { intros h_acyc X X_ne,
-      rcases X_ne with ⟨b, b_in⟩,
-      refine ⟨b, b_in, _⟩,
-      
-
-    },
-    { sorry, }, },
+    cases b_in with hb₁ hb₂,
+    rw trans_gen.tail'_iff at hb₂,
+    rcases hb₂ with ⟨c, hc₁, hc₂⟩,
+    have c_in : c ∈ S,
+    { simp only [S, true_and, sep_def, mem_filter, mem_univ, and_self],
+      split, exact trans_gen.head hc₂ hb₁,
+      exact trans_gen.trans_left hx hc₁, },
+    specialize hb c c_in,
+    exact hc₂.2 hb, },
+  { intros h_acyc X X_ne,
+    by_contradiction h,
+    apply h_acyc,
+    suffices : ∃ c ∈ X, trans_gen (P R) c c,
+    { rcases this with ⟨c, c_in, hc⟩,
+      exact ⟨c, hc⟩, },
+    apply cyclical_of_no_highest (P R) X X_ne,
+    unfold is_best_element at h,
+    push_neg at h, 
+    intros a a_in,
+    specialize h a a_in,
+    rcases h with ⟨b, b_in, hb⟩,
+    exact ⟨b, b_in, ⟨(htot a b).resolve_left hb, hb⟩⟩, },
 end
